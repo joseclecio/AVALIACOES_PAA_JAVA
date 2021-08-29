f44abff8171ed7222e62cef19bf068bcb57767ca
Joseclecio <joseclecio.conceicao96@academico.ifs.edu.br>
2021-08-29 18:29:55 -0300

    Commit de atualização 2

---
+++ src/Avaliacao2_PAA/EstradasEscuras.java
@@ -0,0 +1,113 @@
+package Avaliacao2_PAA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class EstradasEscuras { // uri 1152 - Árvore geradora mínima (Algoritmo de Kruskal)
+
+    static int n, m;
+    static ArrayList<Aresta> listaDeArestas;
+
+    //aqui o algoritmo de kruskal foi instanciado com outras classes que esstão definicdas no código como a de AcharUniao
+    //que é onde irá buscar uma união entre as variáveis Do e para, a fim de definir um único valor
+    static long kruskal() {
+
+        long ans = 0;
+        Collections.sort(listaDeArestas);//operar para retornar a coleção
+        AcharUniao acharUniao = new AcharUniao(n);//instancia da busca da união dos valores
+
+        int ind = 0;
+        while (acharUniao.conjuntoDeNumeros > 1 && ind < listaDeArestas.size()) {
+            Aresta cur = listaDeArestas.get(ind++);
+            int Do = cur.Do, para = cur.para;
+            if (!acharUniao.isSameSet(Do, para)) {
+                acharUniao.uniao(Do, para);
+                ans += cur.custo;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            if (n == 0)
+                break;
+            listaDeArestas = new ArrayList<>(m);
+            long total = 0;
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                total += c;
+                listaDeArestas.add(new Aresta(a, b, c));
+                listaDeArestas.add(new Aresta(b, a, c));
+            }
+            sb.append(total - kruskal()).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    //a lista de arestas foi usada para representar um gráfico como uma lista de suas arestas.
+    static class Aresta implements Comparable<Aresta> {
+        int Do, para, custo;
+
+        public Aresta(int f, int t, int c) {
+            Do = f;
+            para = t;
+            custo = c;
+        }
+
+        public int compareTo(Aresta o) {
+            return custo - o.custo;
+        }
+    }
+
+
+    static class AcharUniao {
+        int[] definir, classificar;
+        int conjuntoDeNumeros;
+
+        public AcharUniao(int n) {
+            conjuntoDeNumeros = n;
+            definir = new int[n];
+            classificar = new int[n];
+            for (int i = 0; i < classificar.length; i++) {
+                definir[i] = i;
+                classificar[i] = 1;
+            }
+        }
+
+        public int achar(int x) {
+            return (definir[x] == x) ? x : (definir[x] = achar(definir[x]));
+        }
+
+        public void uniao(int a, int b) {
+            int x = achar(a), y = achar(b);
+            if (isSameSet(a, b))
+                return;
+            if (classificar[x] > classificar[y])
+                definir[y] = x;
+            else {
+                definir[x] = y;
+                if (classificar[x] == classificar[y])
+                    classificar[y]++;
+            }
+            conjuntoDeNumeros--;
+        }
+
+        public boolean isSameSet(int a, int b) {
+            return achar(a) == achar(b);
+        }
+    }
+}
--- src/Avaliacao2_PAA/ReservaNoHotel.java
+++ src/Avaliacao2_PAA/ReservaNoHotel.java
@@ -10,64 +10,63 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class ReservaNoHotel { //uri 1640 - Algoritmo de Kruskal
+public class ReservaNoHotel { //uri 1640 - Busca de menor Caminho (Algoritmo de dijkstra)
 
-    static final int INF = (int)1e9;
+    static final int INF = (int) 1e9;
 
-    static int dijkstra(ArrayList<Edge>[] listaAdjacente, boolean[] hotel, int V, int T)
-    {
+    static int dijkstra(ArrayList<Aresta>[] listaAdjacente, boolean[] hotel, int V, int T) {
+
+        //utilização de uma matriz de adjacencia
         int[][] distancia = new int[601][V];
-        for(int i = 0; i <= 600; ++i)
+
+        for (int i = 0; i <= 600; ++i)
             Arrays.fill(distancia[i], INF);
+
         PriorityQueue<Estado> pq = new PriorityQueue<Estado>();
         distancia[600][0] = 0;
         pq.add(new Estado(0, 600, 0));
-        while(!pq.isEmpty())
-        {
+
+        while (!pq.isEmpty()) {
             Estado cur = pq.remove();
-            if(cur.cidade == T)
+            if (cur.cidade == T)
                 return cur.custo;
-            if(cur.custo > distancia[cur.resistencia][cur.cidade])
+            if (cur.custo > distancia[cur.resistencia][cur.cidade])
                 continue;
             //Vai para outra cidade
-            for(Edge nxt: listaAdjacente[cur.cidade])
-            {
+            for (Aresta nxt : listaAdjacente[cur.cidade]) {
                 int nxtStamina = cur.resistencia - nxt.tempo;
-                if(nxtStamina >= 0 && cur.custo < distancia[nxtStamina][nxt.cidade])
+                if (nxtStamina >= 0 && cur.custo < distancia[nxtStamina][nxt.cidade])
                     pq.add(new Estado(nxt.cidade, nxtStamina, distancia[nxtStamina][nxt.cidade] = cur.custo));
             }
 
             //Fique aqui
-            if(hotel[cur.cidade] && cur.custo + 1 < distancia[600][cur.cidade])
+            if (hotel[cur.cidade] && cur.custo + 1 < distancia[600][cur.cidade])
                 pq.add(new Estado(cur.cidade, 600, distancia[600][cur.cidade] = cur.custo + 1));
 
         }
         return -1;
     }
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
-        while(true)
-        {
+        while (true) {
             int C = sc.nextInt();
-            if(C == 0)
+            if (C == 0)
                 break;
             int H = sc.nextInt();
             boolean[] hotel = new boolean[C];
-            while(H-->0)
+            while (H-- > 0)
                 hotel[sc.nextInt() - 1] = true;
-            ArrayList<Edge>[] listaAdjacente = new ArrayList[C];
-            for(int i = 0; i < C; ++i)
-                listaAdjacente[i] = new ArrayList<Edge>();
+            ArrayList<Aresta>[] listaAdjacente = new ArrayList[C];
+            for (int i = 0; i < C; ++i)
+                listaAdjacente[i] = new ArrayList<Aresta>();
             int M = sc.nextInt();
-            while(M-->0)
-            {
+            while (M-- > 0) {
                 int u = sc.nextInt() - 1, v = sc.nextInt() - 1, t = sc.nextInt();
-                listaAdjacente[u].add(new Edge(v, t));
-                listaAdjacente[v].add(new Edge(u, t));
+                listaAdjacente[u].add(new Aresta(v, t));
+                listaAdjacente[v].add(new Aresta(u, t));
             }
 
             out.println(dijkstra(listaAdjacente, hotel, C, C - 1));
@@ -76,72 +75,82 @@ public class ReservaNoHotel { //uri 1640 - Algoritmo de Kruskal
         out.close();
     }
 
-    static class Estado implements Comparable<Estado>
-    {
+    static class Estado implements Comparable<Estado> {
         int cidade, resistencia, custo;
 
-        Estado(int x, int y, int z) { cidade = x; resistencia = y; custo = z; }
+        Estado(int x, int y, int z) {
+            cidade = x;
+            resistencia = y;
+            custo = z;
+        }
 
-        public int compareTo(Estado s) { return custo - s.custo; }
+        public int compareTo(Estado s) {
+            return custo - s.custo;
+        }
     }
 
-    static class Edge
-    {
+    static class Aresta {
         int cidade, tempo;
 
-        Edge(int x, int y) { cidade = x; tempo = y; }
+        Aresta(int x, int y) {
+            cidade = x;
+            tempo = y;
+        }
     }
 
-    static class Scanner
-    {
+    static class Scanner {
         StringTokenizer st;
         BufferedReader br;
 
-        public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
 
-        public String proximo() throws IOException
-        {
+        public String proximo() throws IOException {
             while (st == null || !st.hasMoreTokens())
                 st = new StringTokenizer(br.readLine());
             return st.nextToken();
         }
 
-        public int nextInt() throws IOException {return Integer.parseInt(proximo());}
+        public int nextInt() throws IOException {
+            return Integer.parseInt(proximo());
+        }
 
-        public long nextLong() throws IOException {return Long.parseLong(proximo());}
+        public long nextLong() throws IOException {
+            return Long.parseLong(proximo());
+        }
 
-        public String nextLine() throws IOException {return br.readLine();}
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
 
-        public double nextDouble() throws IOException
-        {
+        public double nextDouble() throws IOException {
             String x = proximo();
             StringBuilder sb = new StringBuilder("0");
             double res = 0, f = 1;
             boolean dec = false, neg = false;
             int start = 0;
-            if(x.charAt(0) == '-')
-            {
+            if (x.charAt(0) == '-') {
                 neg = true;
                 start++;
             }
-            for(int i = start; i < x.length(); i++)
-                if(x.charAt(i) == '.')
-                {
+            for (int i = start; i < x.length(); i++)
+                if (x.charAt(i) == '.') {
                     res = Long.parseLong(sb.toString());
                     sb = new StringBuilder("0");
                     dec = true;
-                }
-                else
-                {
+                } else {
                     sb.append(x.charAt(i));
-                    if(dec)
+                    if (dec)
                         f *= 10;
                 }
             res += Long.parseLong(sb.toString()) / f;
-            return res * (neg?-1:1);
+            return res * (neg ? -1 : 1);
         }
 
-        public boolean ready() throws IOException {return br.ready();}
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
 
 
     }
